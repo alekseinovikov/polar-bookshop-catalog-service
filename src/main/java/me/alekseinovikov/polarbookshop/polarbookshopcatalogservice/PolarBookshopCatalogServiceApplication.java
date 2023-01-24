cfbcd5e933a75b2f1538e876d50fe27b163bb3ec
Aleksei Novikov <novikov.aleksei.92@gmail.com>
2023-01-24 19:00:47 +0200

    Initial commit

---
+++ src/main/java/me/alekseinovikov/polarbookshop/polarbookshopcatalogservice/PolarBookshopCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package me.alekseinovikov.polarbookshop.polarbookshopcatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarBookshopCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolarBookshopCatalogServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/alekseinovikov/polarbookshop/polarbookshopcatalogservice/PolarBookshopCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alekseinovikov.polarbookshop.polarbookshopcatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarBookshopCatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
